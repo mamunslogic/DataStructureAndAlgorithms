f5e6dd7bf8183b9261317de1b6a031d72c3cfbc3
Mamun <mamunslogic@hotmail.com>
2024-08-31 21:51:47 +0600

    First Commit

---
+++ DataStructureAndAlgorithms/Program.cs
@@ -0,0 +1,6 @@
+﻿using DataStructureAndAlgorithms.SortingAlgorithm;
+
+var numbers = new int[] { 1000, 500, 100, 300, 700, 900 };
+
+var selectionSort = new SelectionSort();
+selectionSort.DisplaySelectionSort(numbers);
---
+++ DataStructureAndAlgorithms/SortingAlgorithm/SelectionSort.cs
@@ -0,0 +1,48 @@
+﻿namespace DataStructureAndAlgorithms.SortingAlgorithm
+{
+    internal class SelectionSort
+    {
+        private void DoSort(int[] numbers)
+        {
+            int lastMinNumberIndex = 0;
+            int temp;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[lastMinNumberIndex])
+                    {
+                        lastMinNumberIndex = j;
+                    }
+                }
+
+                if (lastMinNumberIndex != i)
+                {
+                    temp = numbers[i];
+                    numbers[i] = numbers[lastMinNumberIndex];
+                    numbers[lastMinNumberIndex] = temp;
+                }
+            }
+        }
+
+        internal void DisplaySelectionSort(int[] numbers)
+        {
+            Console.WriteLine("Lets do a SELECTION SORT!\n");
+
+            Console.WriteLine("Before Sorting the numbers were:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+
+            DoSort(numbers);
+
+            Console.WriteLine("\nAfter Sorting the numbers are:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+        }
+    }
+}
